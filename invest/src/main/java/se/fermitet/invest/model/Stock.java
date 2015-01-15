c06c7baf3eba4b46362c8f63c91a64e6db23627e
fferm <fredrik@fermitet.se>
2015-01-15 22:26:06 +0100

    2: Stock value object
    Task-Url: http://github.com/fferm/Invest/issues/issue/2
    
    Completed

--- invest/src/main/java/se/fermitet/invest/model/Stock.java
+++ invest/src/main/java/se/fermitet/invest/model/Stock.java
@@ -2,4 +2,69 @@ package se.fermitet.invest.model;
 
 public class Stock {
 
+	private String symbol;
+	private String name;
+
+	public Stock(String symbol) {
+		super();
+		validateSymbol(symbol);
+		this.symbol = symbol;
+	}
+
+	private void validateSymbol(String symbol) {
+		if (symbol == null) throw new IllegalArgumentException("The symbol of a stock cannot be null");
+		if (symbol.length() == 0) throw new IllegalArgumentException("The symbol of a stock cannot be empty");
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public Stock setSymbol(String symbol) {
+		validateSymbol(symbol);
+		this.symbol = symbol;
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Stock setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((symbol == null) ? 0 : symbol.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Stock other = (Stock) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (symbol == null) {
+			if (other.symbol != null)
+				return false;
+		} else if (!symbol.equals(other.symbol))
+			return false;
+		return true;
+	}
+
+	
 }
--- invest/src/test/java/se/fermitet/invest/model/StockTest.java
+++ invest/src/test/java/se/fermitet/invest/model/StockTest.java
@@ -1,16 +1,97 @@
 package se.fermitet.invest.model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class StockTest {
 
 	@Test
-	public void constructor() throws Exception {
-		Stock stock = new Stock();
+	public void testConstructor() throws Exception {
+		Stock stock = new Stock("TST");
 		assertNotNull(stock);
 	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorSymbolCannotBeNull() throws Exception {
+		new Stock(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorSymbolCannotBeEmpty() throws Exception {
+		new Stock("");
+	}
+	
+	@Test
+	public void testSymbolProperty() throws Exception {
+		String SYMBOL = "SYMBOL";
+		
+		Stock stock = new Stock(SYMBOL);
+		assertEquals("getter", SYMBOL, stock.getSymbol());
+		
+		String NEWSYMBOL = "NEW SYMBOL";
+		Stock newStock = stock.setSymbol(NEWSYMBOL);
+		assertEquals("getter after setter", NEWSYMBOL, stock.getSymbol());
+		assertSame("same", stock, newStock);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetterSymbolCannotBeNull() throws Exception {
+		Stock stock = new Stock("TST");
+		stock.setSymbol(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetterSymbolCannotBeEmpty() throws Exception {
+		Stock stock = new Stock("TST");
+		stock.setSymbol("");
+	}
+	
+	@Test
+	public void testNameProperty() throws Exception {
+		Stock stock = new Stock("TST");
+		
+		assertNull("getter before", stock.getName());
+		
+		String newName = "new name";
+		Stock newStock = stock.setName(newName);
+		assertEquals("getter after set", newName, stock.getName());
+		assertSame("same", stock, newStock);
+	}
+	
+	@Test
+	public void testValueObject() throws Exception {
+		String sym1 = "SYM1";
+		String sym2 = "SYM2";
+		String name1 = "name 1";
+		String name2 = "name 2";
+		
+		Stock s1 = new Stock(sym1).setName(name1);
+		Stock s2 = new Stock(sym1).setName(name1);
+		Stock diffSym = new Stock(sym2).setName(name1);
+		Stock diffName = new Stock(sym1).setName(name2);
+		Stock nullName1 = new Stock(sym1).setName(null);
+		Stock nullName2 = new Stock(sym1).setName(null);
+		
+		assertTrue("Equal to itself", s1.equals(s1));
+		assertTrue("Equal to object with same symbol and name", s1.equals(s2));
+		assertTrue("Equal to equal object where both have null names", nullName1.equals(nullName2));
+		
+		assertFalse("Not equal to object with different symbol", s1.equals(diffSym));
+		assertFalse("Not equal to object with different name", s1.equals(diffName));
+		assertFalse("Not equal to object with null name", s1.equals(nullName1));
+		assertFalse("Not equal to object of other class", s1.equals("TST"));
+		assertFalse("Not equal to null", s1.equals(null));
+		
+		assertTrue("Equal objects have same hash", s1.hashCode() == s2.hashCode());
+		assertTrue("Equal objects have same hash (both have null names", nullName1.hashCode() == nullName2.hashCode());
+	}
+
 	
 	
 }
