ae06604e068bb93277a6c4cdf18859cae5de768d
fferm <fredrik@fermitet.se>
2015-01-25 22:59:31 +0100

    StockListViewImpl is now a CustomComponent

--- investWebUi/src/se/fermitet/invest/webui/views/StockListViewImpl.java
+++ investWebUi/src/se/fermitet/invest/webui/views/StockListViewImpl.java
@@ -6,17 +6,17 @@ import se.fermitet.invest.domain.Stock;
 import se.fermitet.invest.presenter.StockListPresenter.StockListView;
 import se.fermitet.vaadin.widgets.POJOTableAdapter;
 
+import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
 
-public class StockListViewImpl extends VerticalLayout implements StockListView {
+public class StockListViewImpl extends CustomComponent implements StockListView {
 
 	private static final long serialVersionUID = 1037623075906362499L;
+	
 	POJOTableAdapter<Stock> stockTableAdapter;
 	Table stockTable;
-
-	static final String CODE_PROP_ID = "symbol";
-	static final String NAME_PROP_ID = "name";
+	private VerticalLayout mainLayout;
 
 	public StockListViewImpl() {
 		super();
@@ -25,8 +25,15 @@ public class StockListViewImpl extends VerticalLayout implements StockListView {
 	}
 	
 	private void init() {
-		setMargin(true);
+		this.mainLayout = new VerticalLayout();
+		this.mainLayout.setMargin(true);
+
+		initStockTable(mainLayout);
 		
+		setCompositionRoot(mainLayout);
+	}
+
+	private void initStockTable(VerticalLayout mainLayout) {
 		stockTableAdapter = new POJOTableAdapter<Stock>(Stock.class, "Aktier");
 		stockTable = stockTableAdapter.getTable();
 		
@@ -35,8 +42,8 @@ public class StockListViewImpl extends VerticalLayout implements StockListView {
 
 		stockTable.setSelectable(true);
 		stockTable.setImmediate(true);
-		
-		addComponent(stockTable);
+
+		mainLayout.addComponent(stockTable);
 	}
 
 	@Override
